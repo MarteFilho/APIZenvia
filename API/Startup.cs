5bf89c4a3abe9516eba5f8c71caf04390ba97de6
MarteFilho <57680222+MarteFilho@users.noreply.github.com>
2020-07-18 17:00:03 -0300

    Correção no swagger

--- API/Startup.cs
+++ API/Startup.cs
@@ -25,7 +25,7 @@ namespace API
             services.AddControllers();
 
             services.AddSwaggerGen(c =>
-            c.SwaggerDoc("v1", new OpenApiInfo { Title = "CopaVale API", Version = "v1" })
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "HacktonZenvia API", Version = "v1" })
             );
         }
 
